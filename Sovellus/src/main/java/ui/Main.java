62c52ca5904e885b687ba872588212e1981e1db1
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2020-05-04 15:20:04 +0300

    Muutoksia jar-tiedoston luontia varten

---
+++ Sovellus/src/main/java/ui/Main.java
@@ -0,0 +1,11 @@
+package ui;
+
+/**
+ * Erillinen main-luokka JavaFX:n riippuvuksien paketoinnin takia.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
